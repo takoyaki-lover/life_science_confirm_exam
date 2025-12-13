0cbc0fd267618954ed9cc1319b96bfaa22294124
takoyaki-lover <ss2004722@hotmail.com>
2025-12-14 00:59:50 +0900

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+function showResult(elementId, isCorrect, correctAnswers) {
+    const res = document.getElementById(elementId);
+    res.style.display = "block";
+    if (isCorrect) {
+        res.innerHTML = "<p class='hantei'>正解！</p>";
+        res.className = "result correct";
+    } else {
+        res.innerHTML = "<p class='hantei'>不正解です。</p><span class='hint'>正解の例: " + correctAnswers.join(", ") + "</span>";
+        res.className = "result incorrect";
+    }
+    res.innerHTML += `<button class="close-btn" onclick="closeResult('${elementId}')">閉じる</button>`
+}
+
+function closeResult(elementId) {
+    const res = document.getElementById(elementId);
+    res.style.display = "none";
+}
